97dd8f029efe8142d8cf3ce5f08dff713616ef85
Andrew Balmakund <ando20005@gmail.com>
2020-11-21 19:12:30 -0500

    Added parking lot owner login, increase and decrease current amount of cars

--- src/flask_run.py
+++ src/flask_run.py
@@ -1,9 +1,12 @@
-from flask import Flask, flash, redirect, render_template, request, url_for, g, Response
+from flask import Flask, flash, redirect, render_template, request, url_for, g, Response, session
 import connectToDB
 
 app = Flask(__name__)
+app.secret_key = "super secret key"
+
 
 global parkingOwnerAuthentication
+parkingOwnerAuthentication = [False]
 
 @app.route("/", methods = ['GET', 'POST'])
 def home():
@@ -16,14 +19,16 @@ def home():
 def login_parkinglotowner():
     if request.method == 'POST':
         if "login" in request.form:
-            print("HERE")
             isValid = connectToDB.findOwner('ParkingLotOwners', request.form['username'], request.form['password'])
             if (isValid == True):
                 print("VALID")
+                parkingOwnerAuthentication[0] = True
+                session['ownerUsername'] = request.form['username']
                 return redirect(url_for('parkinglotowner'))
             else:
                 print("NOT VALID")
-                return redirect(url_for('login_parkinglotowner'))
+                #parkingOwnerAuthentication = False
+                return render_template("login_parkinglotowner.html", error = "INVALID CREDENTIALS" ) 
 
         elif "register" in request.form:
             return redirect(url_for('registerOwner.html'))
@@ -33,8 +38,17 @@ def login_parkinglotowner():
 
 @app.route("/parkinglotowner", methods = [ 'GET', 'POST'])
 def parkinglotowner():
-    
-    return render_template("parkinglotowner.html")
+    if parkingOwnerAuthentication[0] == True:
+        ownerUsername = session.get('ownerUsername', None) 
+
+        if request.method == 'POST':
+            if "enters" in request.form:
+                connectToDB.modifyOwnerCurrentCapacity('ParkingLotOwners', ownerUsername, 1)
+            elif "exits" in request.form:
+                connectToDB.modifyOwnerCurrentCapacity('ParkingLotOwners', ownerUsername, -1)
+        return render_template("parkinglotowner.html", ownerUsername = ownerUsername)
+    else:
+        return redirect(url_for('login_parkinglotowner')) 
 
 @app.route("/register_owner", methods = ['GET', 'POST'])
 def register_owner():
